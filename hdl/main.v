09c37edeadeca066ba93a4d1a7eed73280d659c0
David <dgarci23@nd.edu>
2022-01-29 11:06:01 -0500

    Add more memory parameters to the main system

--- hdl/main.v
+++ hdl/main.v
@@ -1,6 +1,14 @@
 module main
 
-  #(parameter WIDTH = 32)
+  #(
+	  parameter WIDTH 	= 32,
+	  parameter DATA_SIZE 	= 256,
+	  parameter DATA_DEPTH 	= 8,
+	  parameter TEXT_SIZE 	= 256,
+	  parameter TEXT_DEPTH 	= 8,
+	  parameter IO_SIZE 	= 1,
+	  parameter IO_DEPTH 	= 32
+  )
 
   (
         input       CLOCK_50,
@@ -24,19 +32,21 @@ module main
     .MEM_mem_out(data_r)
   );
 
-  data_mem #(.WIDTH(256), .DEPTH(8)) data_mem (
+  data_mem #(.WIDTH(DATA_SIZE), .DEPTH(DATA_DEPTH)) data_mem (
     .clk(CLOCK_50),
     .in(data_w),
-    .addr(data_addr[$clog2(256)-1:0]),
+    .addr(data_addr[$clog2(DATA_SIZE)-1:0]),
     .MemLen(data_len),
     .MemRead(data_r_en),
     .MemWrite(data_w_en),
     .out(data_r)
   );
 
-  text_mem #(.DEPTH(8), .WIDTH(256)) text_mem (
+  text_mem #(.WIDTH(TEXT_SIZE), .DEPTH(TEXT_DEPTH)) text_mem (
     .addr(instr_addr),
     .out(instr)
   );
+
+
 
 endmodule
