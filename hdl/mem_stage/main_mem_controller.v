87bc235f0bb77de3f930e086b9e9a2bd8a53fec4
David <dgarci23@nd.edu>
2022-02-10 20:33:01 -0500

    New main memory controller

---
+++ hdl/mem_stage/main_mem_controller.v
@@ -0,0 +1,12 @@
+module main_mem_controller
+
+	(
+		inout			[31:0]			DATA_DQ,
+		output			[19:0]			SRAM_ADDR,
+		output						SRAM_CE_N, SRAM_OE_N, SRAM_WE_N, SRAM_UB_N, SRAM_LB_N,
+		input						clk
+	);
+
+	
+
+endmodule
