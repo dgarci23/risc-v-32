918574f8e976a82aa6edeb5fc2e71e02e9654125
David Garcia Gonzalez <dgarci23@nd.edu>
2022-12-10 13:22:53 -0500

    Prepare test_mem

--- hdl/if_stage/test_mem.v
+++ hdl/if_stage/test_mem.v
@@ -14,11 +14,7 @@ module test_mem
 	reg [DEPTH-1:0] text [WIDTH-1:0];
 	reg [$clog2(WIDTH):0] waddr;
 	
-
-	
-	//initial begin $readmemb("text.mem", text, 0, 255); end
 	initial begin $readmemb("hdl/if_stage/text.mem", text, 0, 255); end
-	
 
 	assign out = {text[raddr+3], text[raddr+2], text[raddr+1], text[raddr]};
 	
