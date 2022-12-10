7f7e6083c5a6de9ce16a595e82fd3b4d55b49f40
David Garcia Gonzalez <dgarci23@nd.edu>
2022-12-10 18:07:49 -0500

    Add writing data to test mem

--- hdl/if_stage/test_mem.v
+++ hdl/if_stage/test_mem.v
@@ -1,25 +1,45 @@
 module test_mem 
 	
-	#(parameter DEPTH = 8, parameter WIDTH = 128)
+	#(parameter DEPTH = 8, parameter WIDTH = 128, parameter MODE="test")
 	
 	(
-		input			[4*DEPTH-1:0]			raddr,
-		//input										clk,
-		//input			[DEPTH-1:0]				in,	
-		//input										we,
-		//input										rst,
-		output 		[4*DEPTH-1:0]			out
+		input			[4*DEPTH-1:0]			addr,
+		input									clk,
+		input			[4*DEPTH-1:0]			in,	
+		input			[3:0]					byteen,
+		input									we,
+		output 		[4*DEPTH-1:0]				out
 	);
 	
-	reg [DEPTH-1:0] text [WIDTH-1:0];
-	reg [$clog2(WIDTH):0] waddr;
+	reg [4*DEPTH-1:0] text [WIDTH-1:0];
 	
+	initial begin 
+		if (MODE=="test") begin
+			$readmemb("hdl/if_stage/text.mem", text, 0, WIDTH-1);
+		end else begin
+			for (int i =0; i < WIDTH; i++) begin
+				text[i] = 0;
+			end
+		end
+	end
 
-	
-	//initial begin $readmemb("text.mem", text, 0, 255); end
-	initial begin $readmemb("hdl/if_stage/text.mem", text, 0, 255); end
-	
+	assign out = text[addr];
 
-	assign out = {text[raddr+3], text[raddr+2], text[raddr+1], text[raddr]};
+	always @(posedge clk) begin
+		if (we) begin
+			if (byteen[0]) begin
+				text[addr][0+:8] <= in[7:0];
+			end
+			if (byteen[1]) begin
+				text[addr][8+:8] <= in[15:8];
+			end
+			if (byteen[2]) begin
+				text[addr][16+:8] <= in[23:16];
+			end
+			if (byteen[3]) begin
+				text[addr][24+:8] <= in[31:24];
+			end
+		end	
+	end
 	
 endmodule
