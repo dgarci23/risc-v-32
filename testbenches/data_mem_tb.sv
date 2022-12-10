25b144a1be163b7a6030d8b42990b5d6202c57aa
David Garcia Gonzalez <dgarci23@nd.edu>
2022-12-10 13:15:25 -0500

    data_mem testbench update

--- testbenches/data_mem_tb.sv
+++ testbenches/data_mem_tb.sv
@@ -1,5 +1,4 @@
 `timescale 1ns/1ns
-`include "./hdl/mem_stage/data_mem.v"
 module data_mem_tb();
 
   reg clk, MemWrite, MemRead;
@@ -21,12 +20,34 @@ module data_mem_tb();
   always #1 clk = ~clk;
 
   initial begin
-    clk = 1; MemWrite = 0; MemRead = 1; MemLen = 0;
-    #1; addr = 152;
-    #2; $display("%h", out);
-    #2; MemWrite = 1; in = 1;
-    #6; $display("%h", out);
+    clk = 1; MemWrite = 0; MemRead = 1; in=32'h7FFFFF7F; addr = 8'b0000_0000;
+    // First case: LB, addr 00, expected sign_ex (0) byteen (0001)
+    MemLen = 2'b01;
+    #2; 
+    print_status();
+    // Second case: LH, addr 00, expected sign_ex (1) byteen (0011)
+    MemLen = 2'b10;
+    #2;
+    print_status();
+    // Third case: LH, addr 10, expected sign_ex (0) byteen (1100)
+    MemLen = 2'b10;
+    addr = 8'b0000_0010;
+    #2;
+    print_status();
+    // First case: LB, addr 00, expected sign_ex (1) byteen (0100)
+    MemLen = 3'b101;
+    #2; 
+    print_status();
 
     $finish;
   end
+
+  function void print_status();
+    $display("Sign Extension: %h", uut.sign_ex);
+    $display("Byte Enable: %b", uut.byteen);
+    $display("RAM in: %h", uut.ram_in);
+    $display("Out: %h", uut.out);
+    $display();
+  endfunction
+
 endmodule
